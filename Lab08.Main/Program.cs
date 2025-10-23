14d7c4343502137dcb2edbbe44c4050aa828a850
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-10-22 20:49:12 -0600

    Initial game uploaded

---
+++ Lab08.Main/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab08;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+        Map map = new Map(4, 4);
+        Player player = new Player(new Location(0, 0));
+        FountainOfObjectsGame game = new FountainOfObjectsGame(map, player);
+        game.Run();
+    }
+}
